3bf41b1d57d21a25581a2cc705cfbce1a48a32a9
BrianFaviansa <brianfpd31@gmail.com>
2024-04-17 12:28:38 +0700

    [feat] finish

--- handle.php
+++ handle.php
@@ -2,7 +2,7 @@
 session_start();
 require_once "function.php";
 
-if (isset($_POST["aksi"]))
+if (isset($_POST["aksi"])) {
   if ($_POST["aksi"] == "add") {
     $sukses = store($_POST);
     if ($sukses) {
@@ -11,15 +11,16 @@ if (isset($_POST["aksi"]))
     } else {
       echo $stmt;
     }
-  } else if ($_POST["aksi"] == "edit") {
-    $sukses = update($_POST);
-    if ($sukses) {
-      $_SESSION["alert"] = "Data berhasil diperbarui";
-      header("Location: index.php");
-    } else {
-      echo $stmt;
-    }
   }
+} else if ($_POST["aksi"] == "edit") {
+  $sukses = update($_POST);
+  if ($sukses) {
+    $_SESSION["alert"] = "Data berhasil diperbarui";
+    header("Location: index.php");
+  } else {
+    echo $stmt;
+  }
+}
 
 if (isset($_GET["hapus"])) {
   $sukses = destroy($_GET);
